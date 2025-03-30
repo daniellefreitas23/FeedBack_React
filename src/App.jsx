f1b5cb85feefe915a8748c2962fca6a4b0ca1ad6
danyrfreitas <danyrfreitas@gmail.com>
2025-03-30 21:10:43 +0000

    fim do segundo card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import iconStar from "./imgs/icon-star.svg"
+import illustrationThankYou from "./imgs/illustration-thank-you.svg"
 
 export function App() {
   const [notaAvaliacao, setNotaAvaliacao] = useState(0)
@@ -44,7 +45,14 @@ export function App() {
       </div>
       ) : (
         <div className="bg-gradient-dark mx-6 p-6 rounded-2xl text-white font-overpass">
-          <p>{notaAvaliacao}</p>
+          <img className="mx-auto mb-6" src={illustrationThankYou} alt="illustration thank you" />
+
+          <p className="text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl mb-6"> You selected {notaAvaliacao} out of 5</p>
+
+          <h1 className="text-2xl font-bold mb-2.5 text-center">Thank you!</h1>
+
+          <p className="text-light-grey text-sm leading-1 text-center">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
+
         </div>
       )
   )
