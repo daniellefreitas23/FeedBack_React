bc56c7d874bff9f5aeb635108607a231378d753a
danyrfreitas <danyrfreitas@gmail.com>
2025-04-21 19:26:30 +0000

    criaçao comp. card final e transição

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
-import illustrationThankYou from "./imgs/illustration-thank-you.svg"
 import { CardInicial } from "./components/CardInicial"
+import { CardIFinal } from "./components/CardFinal"
 
 export function App() {
   const [notaAvaliacao, setNotaAvaliacao] = useState(0)
@@ -10,15 +10,7 @@ export function App() {
     submited === false ? (
         <CardInicial setNotaAvaliacao={setNotaAvaliacao} notaAvaliacao={notaAvaliacao} setSubmited={setSubmited}/>
       ) : (
-        <div className="bg-gradient-dark mx-6 p-6 rounded-2xl text-white font-overpass max-w-103">
-          <img className="mx-auto mb-6" src={illustrationThankYou} alt="illustration thank you" />
-
-          <p className="text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl mb-6"> You selected {notaAvaliacao} out of 5</p>
-
-          <h1 className="text-2xl font-bold mb-2.5 text-center">Thank you!</h1>
-
-          <p className="text-light-grey text-sm leading-1 text-center">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
-        </div>
+        <CardIFinal notaAvaliacao={notaAvaliacao} />
       )
   )
 }
